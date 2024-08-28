349f0c917920e2914daeb68ee69b633a2e5b6dff
Gabriel <gabrielpiraja@hotmail.com>
2024-08-28 22:29:33 +0000

    Fix/bug_build_tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,8 @@ module.exports = {
       'white': '#e3f0ff',
       'red': '#e91e63',
       'green': '#4caf50',
+      'purple': '#351E46',
+      'purple-light': '#5D4370'
     },
     fontFamily: {
       'cursive': ['Shantell Sans', 'cursive'],
